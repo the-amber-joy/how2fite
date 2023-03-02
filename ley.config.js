c5209a550229d951cdc5ff9ef1976d6cdb25f42e
Amber Joy <amber.joy.dev@gmail.com>
2023-03-01 22:09:20 -0600

    migrations

--- ley.config.js
+++ ley.config.js
@@ -1,8 +1,11 @@
-const dotenv = require('dotenv')
+// const dotenv = require('dotenv')
 const { parse } = require('pg-connection-string')
 
-dotenv.config({ path: '.env.local' })
+// dotenv.config({ path: '.env.local' })
+// attaching a fly database uses ipv6 for the host which apparently breaks things
+const dbString = process.env.DATABASE_URL;
+const connectionString = dbString.replace( /\[.*]/, 'how2fite-db.internal');
 
-const options = parse(process.env.DATABASE_URL || '')
+const options = parse(connectionString || '')
 
 module.exports = options
