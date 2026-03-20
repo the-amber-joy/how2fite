0bd27824108e05fa9ecd57e9a970ddc4b534549e
Amber Joy <13399728+the-amber-joy@users.noreply.github.com>
2026-03-20 11:38:46 -0500

    Update ley.config.js

--- ley.config.js
+++ ley.config.js
@@ -2,8 +2,7 @@ const dotenv = require('dotenv')
 const { parse } = require('pg-connection-string')
 
 dotenv.config({ path: '.env.local' })
-const dbString = process.env.DATABASE_URL
-const connectionString = dbString.replace( /\[.*]/, 'how2fite-db.internal');
-const options = parse(connectionString || '')
+const dbString = process.env.NETLIFY_DATABASE_URL
+const options = parse(dbString || '')
 
 module.exports = options
